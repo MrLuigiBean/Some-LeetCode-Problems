099b2367b166033a281c3fe3e8a95dc67a847102
Prashanth <84760999+MrLuigiBean@users.noreply.github.com>
2022-07-23 22:32:14 +0800

    Create 26_Remove_Duplicates_from_Sorted_Array.cpp

---
+++ 26_Remove_Duplicates_from_Sorted_Array.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        /*const size_t oldSize{nums.size()};
+        int newSize = oldSize;
+        for (int i{0}; i < oldSize - 1; ++i)
+        {
+            if (nums[i] == nums[i + 1])
+            {
+                // do the shifting here
+                for (int j{i}; j < oldSize - i; ++j)
+                {
+                    nums[j] = nums[j + 1];
+                }
+                --newSize;
+                i += 1;
+            }
+            if (i == newSize +1)
+            { break; }
+        }
+        return newSize;*/
+        auto x = std::unique(nums.begin(), nums.end());
+        auto y = x - nums.begin();
+        return y;
+        
+    }
+};
