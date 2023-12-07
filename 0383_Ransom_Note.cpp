6ada39a975d57d5bc4a6f5a3d550445fa11e082a
MrLuigiBean <84760999+MrLuigiBean@users.noreply.github.com>
2023-12-07 16:31:39 +0800

    [Improvement!]0383: Ransom Note

--- 0383_Ransom_Note.cpp
+++ 0383_Ransom_Note.cpp
@@ -1,23 +1,32 @@
-// Runtime: 50ms, Beats 5.05% of users with C++
-// Memory: 9.10MB, Beats 85.17% of users with C++
+// Runtime: 13ms, Beats 74.65% of users with C++
+// Memory: 9.00MB, Beats 85.17% of users with C++
 #include <string>
-#include <algorithm>
 
 class Solution
 {
 public:
 	bool canConstruct(std::string ransomNote, std::string magazine)
 	{
-		std::sort(ransomNote.begin(), ransomNote.end());
-		std::sort(magazine.begin(), magazine.end());
+		// If `magazine` has fewer letters than `ransomNote`,
+		// it's not possible for `ransomNote` to be constructed.
+		if (ransomNote.size() > magazine.length())
+			return false;
 
+		// Since `ransomNote` and `magazine` consist of only lowercase English letters,
+		// we can use an array of 26 to allow each alphabet to have its own frequency!
+		int arr[26] = { 0 };
+
+		// 'a' takes the first element in the array
+		for (int i = 0; i < magazine.length(); ++i)
+			++arr[magazine[i] - 'a'];
+
+		// Subtract the frequency of the current `ransomNote` letter.
+		// If the frequency becomes negative after subtraction, there
+		// isn't enough letters to make `ransomNote` from `magazine`.
 		for (int i = 0; i < ransomNote.size(); ++i)
-		{
-			if (auto pos = magazine.find(ransomNote[i]); pos != std::string::npos)
-				magazine.erase(pos, 1);
-			else
+			if (--arr[ransomNote[i] - 'a'] < 0)
 				return false;
-		}
+
 		return true;
 	}
 };
